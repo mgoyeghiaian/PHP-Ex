6983455ba25433dc67d3bb734e406964551e6d69
mgoyeghiaian <mgoyeghiaian@gmail.com>
2023-02-18 22:26:08 +0200

    Basic-01 Step_8

--- Basics-01/Step_8.php
+++ Basics-01/Step_8.php
@@ -6,3 +6,20 @@ echo "Hello Sir, do you have " . $items[0] . ", " . $items[1] . ", and " . $item
 if (in_array("Watermelon",$items))
 echo "Also, if you sell fruits can I find a ". end($items);
 ?>
+//2nd Ex
+-.....................................-
+PHP Data types - Multidimensional Array 
+_....................................._
+<?php
+$items = array(
+"Eggs" => array("Balade","Mazere3"),
+ "Milk"=>array("Fresh","Taanayel"), 
+ "Cheese", 
+ "Water Pack" =>array("Tanoureen","Reem"), 
+ "Tissues" , "Watermelon");
+
+ echo "Hey Sir, Please I need 1 pack of" . $items['Eggs'][0] . " eggs and 3 " . $items['Water Pack'][1] . " Water Pack.";
+
+
+
+?>
